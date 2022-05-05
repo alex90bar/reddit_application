c92e702782e8bd9f683fdef1893ca493ca39eab4
alex90bar <aleksandr.baranov@skillbox.ru>
2022-05-05 12:32:23 +0300

    API to make up- and downvotes realised, TimeAgo library integrated to show last activity time

--- src/main/java/com/example/reddit/repository/VoteRepository.java
+++ src/main/java/com/example/reddit/repository/VoteRepository.java
@@ -1,10 +1,15 @@
 package com.example.reddit.repository;
 
+import com.example.reddit.model.Post;
+import com.example.reddit.model.User;
 import com.example.reddit.model.Vote;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VoteRepository extends JpaRepository<Vote, Long> {
+
+  Optional<Vote> findTopByPostAndUserOrderByVoteIdDesc(Post post, User currentUser);
 
 }
